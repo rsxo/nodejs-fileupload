2869eef67009367087e4ad9f203cc0be4196fb5f
rsxo <lrequejor@gmail.com>
2013-07-07 00:57:41 +0000

    change port

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ function start(route, handle){
 		route(handle, pathname, response, request);
 	}
 
-	http.createServer(onRequest).listen(8888);
+	var port = process.env.PORT || 5000;
+	http.createServer(onRequest).listen(port);
 //	app.listen(8888, function(){
 	console.log("Server has started");
 //	});
